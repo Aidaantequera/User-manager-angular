4fc5220f331a08bc397af86cbc0c96c63ba9bf17
Aida_antequera <aidaantequera8@gmail.com>
2025-03-22 22:16:10 +0100

    Creación formulario
    Creación de formulario para introducir los datos que indica la Tarea

--- src/app/pages/user-form/user-form.component.ts
+++ src/app/pages/user-form/user-form.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormGroup, NgModel, ReactiveFormsModule } from '@angular/forms';
+import { IUsers } from '../../interfaces/iusers';
 
 @Component({
   selector: 'app-user-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.css'
 })
 export class UserFormComponent {
+  @Input() idUser: string = ""
+  UserForm: FormGroup = new FormGroup({}, [])
+  users!: IUsers;
 
+  getDataForm(){
+
+  }
 }
