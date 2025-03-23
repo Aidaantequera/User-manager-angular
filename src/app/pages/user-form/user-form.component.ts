4c4d265a8d780de53658b8f0b08d847da415e185
Aida_antequera <aidaantequera8@gmail.com>
2025-03-23 11:26:37 +0100

    Datos del formulario
    Compruebo que los campos del formulario se registran

--- src/app/pages/user-form/user-form.component.ts
+++ src/app/pages/user-form/user-form.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
-import { FormGroup, NgModel, ReactiveFormsModule } from '@angular/forms';
+import { Component, Input, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { IUsers } from '../../interfaces/iusers';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-user-form',
@@ -9,11 +10,37 @@ import { IUsers } from '../../interfaces/iusers';
   styleUrl: './user-form.component.css'
 })
 export class UserFormComponent {
-  @Input() idUser: string = ""
-  UserForm: FormGroup = new FormGroup({}, [])
-  users!: IUsers;
+  @Input() idUser: string = "";
+  UserForm!: FormGroup;
+  usersService = inject(UsersService); 
+  user!: IUsers; 
+  title: string = "Guardar";
 
-  getDataForm(){
+  async ngOnInit() {
+    if (this.idUser) {
+      try {
+        this.user = await this.usersService.getById(this.idUser);
+        this.title = 'Actualizar'
+        
+      } catch (error) {
+        console.error("Error al obtener usuario:", error);
+      }
+    } else {
+      this.user = {} as IUsers; // Evitamos errores si no hay ID
+    }
 
+    
+    this.UserForm = new FormGroup({
+      _id: new FormControl(this.user._id || ""),
+      first_name: new FormControl(this.user.first_name || ""),
+      last_name: new FormControl(this.user.last_name || ""),
+      username: new FormControl(this.user.username || ""),
+      email: new FormControl(this.user.email || ""),
+      image: new FormControl(this.user.image || ""),
+    });
+  }
+
+  getDataForm() {
+    console.log("Formulario enviado:", this.UserForm.value);
   }
 }
